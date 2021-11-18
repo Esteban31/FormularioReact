6ae817577bd302b16da0f8827308ddfb51f4cb89
Juan Esteban Calle <jecalle58@misena.edu.co>
2021-11-18 00:01:05 -0500

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Formulario from "./components/form";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo"/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        
         </a>
+        <Formulario />
       </header>
     </div>
   );
